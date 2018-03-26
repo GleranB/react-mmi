a35d24d08ad24d707b8eaaec80744c76ab529693
Beherec Gleran <gleran.beherec@etu.univ-grenoble-alpes.fr>
2018-03-26 08:50:16 +0000

    bouton

---
+++ app/components/Bouton.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * App.js
+ *
+ */
+import React from 'react';
+
+class Bouton extends React.Component {
+  render() {
+    // Dans tous les cas, afficher
+    return <button type="button">Click Me On</button>
+          
+  }
+}
+
+// Le composant sera accessible avec le nom "Bouton"
+export default Bouton;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,13 +7,22 @@ import React from 'react';
 
 // On importe ici les composants qu'on veut afficher
 import Machine from '../../components/Machine.js';
+import Bouton from '../../components/Bouton.js';
+
 // On peut aussi importer du CSS de la meme facon.
+ import '../../css/styles.css';
 
 export default function App() {
   return (
     // Un return doit retourner un seul élément du DOM
     // Si on veut afficher plusieurs éléments adjacents,
     // On devra donc les encapsuler dans une DIV parente.
+    <div> 
     <Machine />
+    <Machine />
+    <Machine />
+    <Bouton />
+    </div>
+    
   );
 }
